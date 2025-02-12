28147792f43dd015ca1a411dd94b73d421cf0d51
R2Jesu <r2jesu2019@gmail.com>
2025-02-11 20:57:06 -0600

    container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,34 @@
 
 package frc.robot;
 
+import java.io.File;
+
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
 public class RobotContainer {
+
+   // Replace with CommandPS4Controller or CommandJoystick if needed
+  final         CommandXboxController driverXbox = new CommandXboxController(0);
+  // The robot's subsystems and commands are defined here...
+  private final SwerveSubsystem       drivebase  = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
+                                                                                "swerve"));
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    drivebase.setDefaultCommand(
+      drivebase.driveCommand(() -> -driverXbox.getRightY(),
+        () ->  -driverXbox.getRightX(),
+        () -> -driverXbox.getLeftX()));
+
+    driverXbox.button(2).whileTrue(drivebase.aimAtSpeaker(0));
+    driverXbox.button(1).whileTrue(drivebase.aimAtTarget());
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
